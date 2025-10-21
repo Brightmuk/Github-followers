e555a17dc18e562f189c2de196174351c0f49c43
Bright Mukonesi <48471360+Brightmuk@users.noreply.github.com>
2025-10-21 23:55:27 +0300

    Setup tab view

---
+++ Github-fl/FavoritesVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesVC.swift
+//  Github-fl
+//
+//  Created by Bright Mukonesi on 21/10/2025.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+}
---
+++ Github-fl/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  Github-fl
+//
+//  Created by Bright Mukonesi on 21/10/2025.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
