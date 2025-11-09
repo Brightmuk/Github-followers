e6268578f4c2f533f089dd1c3e971edcb0a72386
Bright Mukonesi <48471360+Brightmuk@users.noreply.github.com>
2025-11-09 23:24:50 +0300

    Network handler

--- Github-fl/Managers/NetworkManager.swift
+++ Github-fl/Managers/NetworkManager.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+class NetworkManager{
+    static let shared = NetworkManager()
+    let baseURl = "https://api.github.com/users/"
+    private init(){}
+    
+    func getFollowers(for username: String, page: int, completed: @escaping ([Follower]?, String?) -> Void){
+        let endpoint = baseURl + "users/\(username)followers?per_page=100&page=\(page)"
+        
+        guard let url = URL(string: endpoint) else{
+            completed(nil, "This username created an invalid request, please try again")
+        }
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error{
+                print("Error fetching data: \(error)")
+                completed(nil, "Error fetching data, please try again")
+                return
+            }
+            guard let data = data else{
+                print("No data returned from server")
+                completed(nil, "No data returned from server")
+                return
+            }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+                let reason: String
+
+                reason = "Server returned status code other than 200"
+
+                reason = "Server returned status code other than 200"
+
+                completed(nil, reason)
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                completed(followers, nil)
+                
+            }catch{
+                completed(nil, "THe data received was not in the correct format")
+            }
+          
+        }
+        task.resume()
+    }
+}
--- Github-fl/Screens/SearchVC.swift
+++ Github-fl/Screens/SearchVC.swift
@@ -25,7 +25,7 @@ class SearchVC: UIViewController {
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.isNavigationBarHidden  = true
+        navigationController?.setNavigationBarHidden(true, animated: true)
     }
     func createDismissKeyboardTapGesture(){
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
